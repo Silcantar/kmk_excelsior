cfcabb3009d3817bdb5b6bf82a00abaabc2e06d5
Silcantar <joshdlucas@gmail.com>
2025-06-21 21:23:34 -0500

    Add code to install script to copy dependencies from submodules to the locations required by CircuitPython.

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
+mkdir ./lib/
+
+rsync -av ./submodules/kmk_firmware/kmk/ ./kmk/
+rsync -av ./submodules/Adafruit_CircuitPython_MCP230xx/adafruit_mcp230xx/ ./lib/adafruit_mcp230xx/
+rsync -av ./submodules/Adafruit_CircuitPython_SSD1322/adafruit_ssd1322.py ./lib/adfruit_ssd1322.py
+
 rsync -avm --delete --filter='merge install.rules' ./ /media/$USER/EXCELSIOR/
